e610e2c117c20a7297ad437579842b7ddbfd83b2
Mike Borthwick <mhborthwick@gmail.com>
2026-05-05 00:59:07 -0700

    set up init script

---
+++ init.sh
@@ -0,0 +1,4 @@
+# Note: Symlinks assume ~/Workspace directory exists
+ln -sf /Users/$USER/Workspace/dotfiles/ghostty /Users/$USER/.config/ghostty
+ln -sf /Users/$USER/Workspace/dotfiles/git/.gitconfig /Users/$USER/.gitconfig
+
