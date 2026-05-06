0971ad207688334f01fc2298519de3ebbbdbb598
Mike Borthwick <mhborthwick@gmail.com>
2026-05-05 23:52:14 -0700

    set git aliases

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 # Note: Symlinks assume ~/Workspace directory exists
 ln -sf /Users/$USER/Workspace/dotfiles/ghostty /Users/$USER/.config/ghostty
 ln -sf /Users/$USER/Workspace/dotfiles/git/.gitconfig /Users/$USER/.gitconfig
+ln -sf /Users/$USER/Workspace/dotfiles/git/.gitconfig-work /Users/$USER/.gitconfig-work
 
