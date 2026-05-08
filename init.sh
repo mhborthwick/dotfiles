af0713061f4bb8869caf340a2ecf368f2b1bdac6
Mike Borthwick <mhborthwick@gmail.com>
2026-05-07 19:25:37 -0700

    remove took from cmd_duration

--- init.sh
+++ init.sh
@@ -7,3 +7,5 @@ ln -sf /Users/$USER/$WORKSPACE/dotfiles/ghostty /Users/$USER/.config/ghostty
 ln -sf /Users/$USER/$WORKSPACE/dotfiles/git/.gitconfig /Users/$USER/.gitconfig
 ln -sf /Users/$USER/$WORKSPACE/dotfiles/git/.gitconfig-work /Users/$USER/.gitconfig-work
 
+# startship
+ln -sf /Users/$USER/$WORKSPACE/dotfiles/starship/starship.toml /Users/$USER/.config/starship.toml
