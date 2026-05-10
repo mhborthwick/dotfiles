abb1777526bc98509a46aa555cb8f3f68b0a8377
Mike Borthwick <mhborthwick@gmail.com>
2026-05-09 18:41:02 -0700

    add eza aliases

--- aliases/init_aliases.zsh
+++ aliases/init_aliases.zsh
@@ -1,6 +1,12 @@
 # git
 alias g='git'
 
-# commands
+# shortcuts
 alias cl='clear'
 alias x='exit'
+
+# eza
+alias ls='eza --icons --group-directories-first'
+alias ll='eza -lh --git'
+alias la='eza -a'
+alias lt='eza --tree --level=2 --icons --group-directories-first'
