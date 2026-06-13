cd202bea12433f82196433c9be207311e0bc2429
Mike Borthwick <mhborthwick@gmail.com>
2026-06-13 16:07:37 -0400

    add aliases

--- aliases/init_aliases.zsh
+++ aliases/init_aliases.zsh
@@ -13,3 +13,18 @@ alias ls='eza -a --icons --group-directories-first'
 alias ll='eza -la --git'
 alias la='eza -a'
 alias lt='eza -a --tree --level=2 --icons --group-directories-first'
+
+# cursor
+alias c='cursor'
+
+# kubectl
+alias k='kubectl'
+
+# docker
+alias d='docker'
+
+# npm
+alias nr='npm run'
+
+# pnpm
+alias pnr='pnpm run'
